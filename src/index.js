b948a95a7142761f20c41b6b12acff671f5a6261
ryotogashi <ryo.togashi.ca@gmail.com>
2019-08-10 11:57:59 -0700

    change js to ts

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import longestCommonPrefixTest from "./longest_common_prefix/longest_common_pref
 import reverseInteger from "./reverse_integer/reverse_integer_test";
 import romanToIntTest from "./roman_to_integer/roman_to_integer_test";
 import validParenthesesTest from "./valid_parentheses/valid_parentheses_test";
-import margeTwoSortedListTest from "./marge_two_sorted_list/marge_two_sorted_list_test";
+import mergeTwoSortedListTest from "./021_merge_two_sorted_list/merge_two_sorted_list_test";
 
 const main = () => {
-  margeTwoSortedListTest();
+  mergeTwoSortedListTest();
 };
 
 main();
