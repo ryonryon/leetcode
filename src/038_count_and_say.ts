e386881df1735570bfacb2d694c62d50c84aa6b1
ryotogashi <ryo.togashi.ca@gmail.com>
2019-08-19 22:43:54 -0700

    add 038

--- src/038_count_and_say.ts
+++ src/038_count_and_say.ts
@@ -5,34 +5,33 @@
  * @return {string}
  */
 
+function subCountAndSay(str: string): string {
+  return str.length.toString() + str[0];
+}
+
 function countAndSay(n: number): string {
   let ansStr: string = "1";
+  let tempStr: string = "";
   if (n === 1) {
     return ansStr;
   }
 
-  for (let i = 0; i < n; i++) {
-    let tempNum: string | null = null;
-    let countNum: number = 0;
-    let tempAns: string = "";
-
-    for (let j = 0; j < ansStr.length; j++) {
-      if (tempNum === ansStr[i]) {
-        countNum++;
+  for (let i = 0; i < n - 1; i++) {
+    let tempAnsStr = ansStr;
+    ansStr = "";
+    for (let j = 0; j < tempAnsStr.length; j++) {
+      if (tempStr == "") {
+        tempStr = tempAnsStr[j];
+      } else if (tempStr[0] === tempAnsStr[j]) {
+        tempStr += tempAnsStr[j];
       } else {
-        if (0 < countNum) {
-          tempAns = tempAns + tempNum + countNum.toString();
-        }
-
-        tempNum = ansStr[i];
-        countNum = 1;
+        ansStr += subCountAndSay(tempStr);
+        tempStr = tempAnsStr[j];
       }
 
-      if (j === ansStr.length - 1) {
-        if (0 < countNum) {
-          tempAns = tempAns + tempNum + countNum.toString();
-        }
-        ansStr = tempAns;
+      if (j == tempAnsStr.length - 1) {
+        ansStr += subCountAndSay(tempStr);
+        tempStr = "";
       }
     }
   }
@@ -45,6 +44,15 @@ describe("count and say", () => {
     expect(countAndSay(1)).toBe("1");
   });
   test("#2", () => {
+    expect(countAndSay(2)).toBe("11");
+  });
+  test("#3", () => {
+    expect(countAndSay(3)).toBe("21");
+  });
+  test("#4", () => {
     expect(countAndSay(4)).toBe("1211");
+  });
+  test("#5", () => {
+    expect(countAndSay(5)).toBe("111221");
   });
 });
