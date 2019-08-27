551d95ba1236732aafdf16d1483ca53e2d515fe7
ryotogashi <ryo.togashi.ca@gmail.com>
2019-08-26 22:44:32 -0700

    change 204

--- src/204_count_primes.ts
+++ src/204_count_primes.ts
@@ -5,22 +5,6 @@
  * @return {number}
  */
 
-// function isPrime(n: number): boolean {
-//   if (n <= 1) return false;
-//   for (let i = 2; i * i <= n; i++) {
-//     if (n % i == 0) return false;
-//   }
-//   return true;
-// }
-
-// function countPrimes(n: number): number {
-//   let count = 0;
-//   for (let i = 1; i < n; i++) {
-//     if (isPrime(i)) count++;
-//   }
-//   return count;
-// }
-
 function countPrimes(n: number): number {
   const isPrime: boolean[] = [];
   for (let i = 2; i < n; i++) {
