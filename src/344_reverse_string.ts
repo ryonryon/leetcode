581b8bba376c462bc1b17c52da88f75e39edc485
ryotogashi <ryo.togashi.ca@gmail.com>
2019-08-26 23:40:32 -0700

    add 344

---
+++ src/344_reverse_string.ts
@@ -0,0 +1,24 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    const temp: string = s[i];
+    s[i] = s[s.length - 1 - i];
+    s[s.length - 1 - i] = temp;
+  }
+}
+
+describe("reverse string", () => {
+  test("#1", () => {
+    const hello = ["h", "e", "l", "l", "o"];
+    reverseString(hello);
+    expect(hello).toEqual(["o", "l", "l", "e", "h"]);
+  });
+  test("#1", () => {
+    const hannah = ["H", "a", "n", "n", "a", "h"];
+    reverseString(hannah);
+    expect(hannah).toEqual(["h", "a", "n", "n", "a", "H"]);
+  });
+});
